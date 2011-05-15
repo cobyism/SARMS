b5d00e7bc862200dc14eeaa552b506372d8f9354
cobychapple <coby@chapplecreative.com>
2011-05-15 17:54:23 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SARMS::Application.config.secret_token = '34c4ed92a8fdd7653b156dd230bc1cb3a65ee3f9d20e1900b10e1356dd5f65b1219ac836f3685796b3aef369b73dcf0ba444d8d8ddbba6d3932ce9970a794453'
