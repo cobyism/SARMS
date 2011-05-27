b21789c391c9eaeebea6bcdd74539e6dd4ea09bb
cobychapple <coby@chapplecreative.com>
2011-05-27 11:10:20 +1000

    added basic counts to dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,14 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @count = {
+      :users => User.count,
+      :students => User.students.count,
+      :faculty => User.faculty.count,
+      :admins => User.admins.count,
+      :units => Unit.count,
+      }
+    
   end
 
 end
