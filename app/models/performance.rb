d027440f257e9f6495b789dfddab03b4dddd1b37
cobychapple <coby@chapplecreative.com>
2011-05-28 16:50:05 +1000

    fixed spelling mistake in performance model validation

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -2,5 +2,5 @@ class Performance < ActiveRecord::Base
   belongs_to :assessment
   belongs_to :enrollment
   
-  validates_uniqueness_of :enrollent_id, :scope => :assessment_id
+  validates_uniqueness_of :enrollment_id, :scope => :assessment_id
 end
