c7809fbc03177fb1d37a728c8673644a06df7246
cobychapple <coby@chapplecreative.com>
2011-05-29 15:36:29 +1000

    moved sign out link to sidebar and added my account details link

--- app/models/at_risk_event.rb
+++ app/models/at_risk_event.rb
@@ -1,4 +1,8 @@
 class AtRiskEvent < ActiveRecord::Base
   belongs_to :enrollment
   has_many :responses, :dependent => :destroy
+  
+  def response_received?
+    return self.responses.count > 0
+  end
 end
